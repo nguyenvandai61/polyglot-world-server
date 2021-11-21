ce4726b156689174a6c2e3849e1f704de1fb87d9
nguyenvandai61 <nguyenvandai61@users.noreply.github.com>
2021-11-21 17:51:01 +0700

    ✨ Login with JWT token (#1)

--- app/auth/urls.py
+++ app/auth/urls.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.views import (
 )
 
 
-url_patterns = [
+urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
