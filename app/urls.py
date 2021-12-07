2c8cf682db9e98fcfd5dac246aee009575ea942c
nguyenvandai61 <nguyenvandai61@users.noreply.github.com>
2021-12-07 19:47:19 +0700

    ✨ Add api for profile detail

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('auth/', include("app.auth.urls"), name="auth"),
     path('country/', include("app.api.country.urls"), name="country"),
     path('language/', include("app.api.language.urls"), name="language"),
+    path('user/', include("app.api.user.urls"), name="user"),
 ]
